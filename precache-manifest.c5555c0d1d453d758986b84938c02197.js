286828dcaa2b27b9af3fff4602e4505d7af42bd1
Grigor Abrahamyan <g.r.abrahamyan@gmail.com>
2020-07-14 00:29:10 +0400

    Updates

---
+++ precache-manifest.c5555c0d1d453d758986b84938c02197.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa984ea1a2f0a224ca9d62c48d90dbd1",
+    "url": "/grigorabrahamyan/TypeRacer/index.html"
+  },
+  {
+    "revision": "dc6cd49887bde77c0cc1",
+    "url": "/grigorabrahamyan/TypeRacer/static/css/main.7ab18e06.chunk.css"
+  },
+  {
+    "revision": "c0a458a6ef1f56a614f9",
+    "url": "/grigorabrahamyan/TypeRacer/static/js/2.c68f7883.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/grigorabrahamyan/TypeRacer/static/js/2.c68f7883.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6cd49887bde77c0cc1",
+    "url": "/grigorabrahamyan/TypeRacer/static/js/main.0d61a58d.chunk.js"
+  },
+  {
+    "revision": "4dfa9a9b367fb4877980",
+    "url": "/grigorabrahamyan/TypeRacer/static/js/runtime-main.133798d1.js"
+  }
+]);
