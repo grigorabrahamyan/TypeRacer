3193eb060d7071a15570f010b426944f1e24a5b0
Grigor Abrahamyan <g.r.abrahamyan@gmail.com>
2020-07-14 01:23:56 +0400

    all changes

--- src/componnets/main/getUsers.jsx
+++ src/componnets/main/getUsers.jsx
@@ -3,80 +3,51 @@ import Loading from '../../assets/loading';
 
 const GetUsers = () => {
 
-  const [data, setData] = useState([]);
   const [correctData, setCorrectData] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  const finsih = useMemo(() => {
-    let result = [...correctData];
-    if (result.length > 10) {
-      result = result.slice(0, 10);
-    }
-    return result;
-  }, [correctData]);  
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    try {
-      (
-        async () => {
-          setLoading(true);
-          let response = await fetch('https://jsonstorage.net/api/items/916710d9-3147-4623-a0dd-02108c2fe3f0');
-          let result = await response.json();
-          setData(result);
-          setLoading(false);
-        }
-      )()
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (!Array.isArray(data)) {
+    (async function () {
       try {
-        (
-          async () => {
-            setLoading(true);
-            let response = await fetch(data.uri);
-            let result = await response.json();
-            setCorrectData([result]);
-            setLoading(false);
-          }
-        )()
-      } catch (err) {
-        console.log(err);
+        let response = await fetch('https://jsonstorage.net/api/items/916710d9-3147-4623-a0dd-02108c2fe3f0');
+        let result = await response.json();
+        if (!Array.isArray(result)) {
+          result = [result];
+        }
+        const promiseArray = result.map(item => fetch(item.uri));
+        const responses = await Promise.all(promiseArray);
+        const data = await Promise.all(responses.map(item => item.json()));
+        let resultData = [...data];
+        if (resultData.length > 10) {
+          resultData = resultData.slice(0, 10);
+        }
+        setCorrectData(resultData);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
       }
-    } else {
-      const correctData = data.map(item => fetch(item.uri));
-      //setLoading(true);
-      Promise.all(correctData)
-        .then(response => response)
-        .then(response => Promise.all(response.map(r => r.json())))
-        .then(response => setCorrectData(response))
-        //.then(response => setLoading(false))
-        .catch(err => console.log(err));
-    }
-  }, [data]);
+    })();
+  }, []);
 
   if (loading) {
     return <Loading />
   }
 
-  return (
-    <div className='startGame__wrapperUsers' >
-      {
-        finsih?.map((item, index) => {
-          return (
-            <div key={index} className='startGame__user' >
-              <p>{`${item.firstName} ${item.lastName}`}</p>
-              <p>{`WPM: ${item.netWPM}`}</p>
-              <p>{`Accurancy: ${item.accuracy}%`}</p>
-            </div>
-          )
-        })
-      }
-    </div>
-  )
+    return (
+      <div className='startGame__wrapperUsers' >
+        {
+          correctData?.map((item, index) => {
+            return (
+              <div key={index} className='startGame__user' >
+                <p>{`${item.firstName} ${item.lastName}`}</p>
+                <p>{`WPM: ${item.netWPM}`}</p>
+                <p>{`Accurancy: ${item.accuracy}%`}</p>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
 };
 
 export default GetUsers;
